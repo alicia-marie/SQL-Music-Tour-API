2e2b6f90c4a96196d3498ffcb2faec6afd54b735
Alicia MacEwan-Gutierrez <alicia7749@gmail.com>
2023-06-26 17:23:22 -0700

    complete assignment

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -1,7 +1,7 @@
 // DEPENDENCIES
 const bands = require('express').Router()
 const db = require('../models')
-const { Band } = db
+const { Band, MeetGreet, Event, SetTime } = db
 const { Op } = require('sequelize')
 
 // ENDPOINTS
@@ -9,7 +9,7 @@ bands.get('/', async (req, res) => {
     try {
         const foundBands = await Band.findAll({
             order: [ ['available_start_time', 'ASC'] ],
-            where: { name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` } }
+            where: { name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` } },
         })
         res.status(200).json(foundBands)
     } catch(err) {
@@ -18,9 +18,30 @@ bands.get('/', async (req, res) => {
     }
 })
 
-bands.get('/:id', async (req, res) => {
+bands.get('/:name', async (req, res) => {
     try {
-        const foundBand = await Band.findOne({ where: {band_id: req.params.id} })
+        const foundBand = await Band.findOne({
+            where: {name: req.params.name},
+            include: [
+                {
+                    model: MeetGreet,
+                    as: 'meet_greets',
+                    include: {
+                        model: Event,
+                        as: 'event',
+                        where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%` } }
+                    }
+                }, {
+                    model: SetTime,
+                    as: 'set_times',
+                    include: {
+                        model: Event,
+                        as: 'event',
+                        where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%` } }
+                    }
+                }
+            ]
+        })
         res.status(200).json(foundBand)
     } catch(err) {
         console.log(err)
